4cc1f30dd68d61b106ffb9eb22f6cdcffe3397c0
WebStaticCS <144613482+WebStaticCS@users.noreply.github.com>
2025-06-11 19:40:35 -0300

    No cache Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ const TIMEZONE_OFFSET_HOURS = parseInt(process.env.TIMEZONE_OFFSET_HOURS || '-3'
 
 const PLACEHOLDER_POSTER = 'https://via.placeholder.com/200x300?text=No+Poster';
 const PLACEHOLDER_BACKGROUND = 'https://via.placeholder.com/1280x720?text=No+Background';
-const EVENTS_JSON_URL = 'https://stream196tp.com/eventos.json';
+const EVENTS_JSON_URL = 'https://stream196tp.com/eventos.json?nocache=1748748608282';
 const SOURCE_TIMEZONE_OFFSET_HOURS = parseInt(process.env.SOURCE_TIMEZONE_OFFSET_HOURS || '-8', 10); 
 
 // Lista de proveedores de stream disponibles
